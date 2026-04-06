7d869aa8f0a57b3d7eb26afd21b68e5e8f8d6d10
sujithgoud2210 <sujith200322@gmail.com>
2026-04-06 12:59:24 +0530

    product repository

--- src/com/stschool/ecommerce/repository/ProductRepository.java
+++ src/com/stschool/ecommerce/repository/ProductRepository.java
@@ -1,5 +1,30 @@
 package com.stschool.ecommerce.repository;
 
+import com.stschool.ecommerce.model.Product;
+import com.stschool.ecommerce.util.CsvParser;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
 public class ProductRepository {
+
+    private final List<Product> products;
+
+    public ProductRepository() throws IOException {
+        CsvParser csvParser = new CsvParser();
+        products = csvParser.getProductsFromCsv();
+    }
+
+    public Product save(Product product) {
+        this.products.add(product);
+        return product;
+    }
+
+    public Optional<Product> getById(int id){
+       return products.stream()
+                .filter(product -> product.getId()==id)
+                .findFirst();
+    }
 
 }
