3e460f6168da8e0dcef3239efbd921e4f82dfd1b
sujithgoud2210 <sujith200322@gmail.com>
2026-04-07 23:08:38 +0530

    productserviceimpl

--- src/com/stschool/ecommerce/service/ProductServiceImpl.java
+++ src/com/stschool/ecommerce/service/ProductServiceImpl.java
@@ -1,5 +1,15 @@
 package com.stschool.ecommerce.service;
 
+import com.stschool.ecommerce.model.Product;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ProductServiceImpl {
+    public List<Product> getAvailableProducts(Product product) {
+        return Product.stream()
+                .filter(Product::isAvailable)
+                .collect(Collectors.toList());
+    }
 
 }
