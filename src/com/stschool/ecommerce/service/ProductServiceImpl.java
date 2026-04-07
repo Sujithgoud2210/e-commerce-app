9c0c2cfe687f75457839d25af007ac12927271bb
sujithgoud2210 <sujith200322@gmail.com>
2026-04-07 23:24:38 +0530

    productserviceimpl

--- src/com/stschool/ecommerce/service/ProductServiceImpl.java
+++ src/com/stschool/ecommerce/service/ProductServiceImpl.java
@@ -5,11 +5,15 @@ import com.stschool.ecommerce.model.Product;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ProductServiceImpl {
-    public List<Product> getAvailableProducts(Product product) {
-        return Product.stream()
-                .filter(Product::isAvailable)
-                .collect(Collectors.toList());
+public class ProductServiceImpl implements ProductService{
+
+    private List<Product> productList;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productList.stream()
+                .collect(Collectors.toList();
     }
+}
 
 }
