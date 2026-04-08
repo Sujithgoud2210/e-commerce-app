eed6389cecd1110481a2b70b556635920f4ed80c
sujithgoud2210 <sujith200322@gmail.com>
2026-04-08 12:40:17 +0530

    productserviceimpl

--- src/com/stschool/ecommerce/service/ProductService.java
+++ src/com/stschool/ecommerce/service/ProductService.java
@@ -1,38 +1,24 @@
 package com.stschool.ecommerce.service;
 
+import com.stschool.ecommerce.exception.ProductExistException;
+import com.stschool.ecommerce.exception.ProductNotFoundException;
 import com.stschool.ecommerce.model.Product;
-
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public interface ProductService {
 
-    public List<Product> getAllProducts();
-    public List<Product> getProductsByCategory(String category);
-    public List<Product> getProductsAbovePrice(double price);
-    public List<String> getAllProductNames();
-    public long countAvailableProducts();
-    public boolean existsByCompany(String company);
-    public boolean areAllProductsAvailable();
-    public Optional<Product> getFirstProduct();
+    Product save(Product product) throws ProductExistException;
 
-    public List<String> getUniqueCategories();
-    public List<Product> getTopNExpensiveProducts(int n);
-    public List<Product> sortByPriceAsc();
-    public List<Product> sortByNameDesc();
-    public double getTotalInventoryValue();
-    public double getTotalPriceAfterDiscount();
-    public List<Product> getProductsAfterYear(int year);
-    public List<Product> getAvailableAndPriceGreater(double price);
-    public Map<String, Long> countByCategory();
+    Product getById(int id) throws ProductNotFoundException;
 
-    public Map<String, List<Product>> groupByCategory();
-    public Map<String, List<Product>> groupByCompany();
-    public Map<Boolean, List<Product>> partitionByAvailability();
-    public Optional<Product> getMostExpensiveProduct();
-    public Optional<Product> getCheapestProduct();
-    public Map<Integer, Product> mapById();
-    public Map<String, Double> avgPriceByCategory();
-    public Map<String, List<Product>> top3ExpensiveByCategory();
+    List<Product> getAll() throws ProductNotFoundException;
+
+    Product update(int id,Product product) throws ProductNotFoundException;
+
+    void delete(int id) throws ProductNotFoundException;
+
+    List<Product> getProductByAvailability(boolean isAvailable);
+
+    List<Product> getProductByCategory();
+
 }
--- src/com/stschool/ecommerce/service/ProductServiceImpl.java
+++ src/com/stschool/ecommerce/service/ProductServiceImpl.java
@@ -1,194 +1,61 @@
 package com.stschool.ecommerce.service;
 
+import com.stschool.ecommerce.exception.ProductExistException;
+import com.stschool.ecommerce.exception.ProductNotFoundException;
 import com.stschool.ecommerce.model.Product;
 import com.stschool.ecommerce.repository.ProductRepository;
 
-import java.io.IOException;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class ProductServiceImpl implements ProductService {
 
-    private List<Product> products; // declare list
+    private final ProductRepository productRepository;
 
-    public ProductServiceImpl() throws IOException {
-        ProductRepository productRepository = new ProductRepository();
+    public ProductServiceImpl(ProductRepository productRepository) {
+
+        this.productRepository = productRepository;
+    }
+
+
+    @Override
+    public Product save(Product product) throws ProductExistException {
+        productRepository.getById(product.getId())
+                .ifPresent((p->{
+                    throw new ProductExistException("Product already exist exception");
+                }));
+                return productRepository.save(product);
     }
 
     @Override
-    public List<Product> getAllProducts() {
-        return products;
+    public Product getById(int id) throws ProductNotFoundException {
+        return productRepository.getById(id)
+                .orElseThrow(()-> new ProductNotFoundException("product not found exception"));
     }
 
     @Override
-    public List<Product> getProductsByCategory(String category) {
-        return products.stream()
-                .filter(p -> p.getCategory().equalsIgnoreCase(category))
-                .toList();
+    public List<Product> getAll() throws ProductNotFoundException {
+        return productRepository.getAll();
     }
 
     @Override
-    public List<Product> getProductsAbovePrice(double price) {
-        return products.stream()
-                .filter(p -> p.getMaxRetailPrice() > price)
-                .toList();
+    public Product update(int id, Product product) throws ProductNotFoundException {
+        return productRepository.getById(id)
+                .orElseThrow(()-> new ProductNotFoundException("product not found exception"));
     }
 
     @Override
-    public List<String> getAllProductNames() {
-        return products.stream()
-                .map(Product::getName)
-                .toList();
+    public void delete(int id) throws ProductNotFoundException {
+        productRepository.getById(id)
+            .orElseThrow(()-> new ProductNotFoundException("product not found exception"));
     }
 
     @Override
-    public long countAvailableProducts() {
-        return products.stream()
-                .filter(Product::isAvailable)
-                .count();
+    public List<Product> getProductByAvailability(boolean isAvailable) {
+        return List.of();
     }
 
     @Override
-    public boolean existsByCompany(String company) {
-        return products.stream()
-                .anyMatch(p -> p.getCompany().equalsIgnoreCase(company));
-    }
-
-    @Override
-    public boolean areAllProductsAvailable() {
-        return products.stream()
-                .allMatch(Product::isAvailable);
-    }
-
-    @Override
-    public Optional<Product> getFirstProduct() {
-        return products.stream().findFirst();
-    }
-
-    @Override
-    public List<String> getUniqueCategories() {
-        return products.stream()
-                .map(Product::getCategory)
-                .distinct()
-                .toList();
-    }
-
-    @Override
-    public List<Product> getTopNExpensiveProducts(int n) {
-        return products.stream()
-                .sorted(Comparator.comparing(Product::getMaxRetailPrice).reversed())
-                .limit(n)
-                .toList();
-    }
-
-    @Override
-    public List<Product> sortByPriceAsc() {
-        return products.stream()
-                .sorted(Comparator.comparing(Product::getMaxRetailPrice))
-                .toList();
-    }
-
-    @Override
-    public List<Product> sortByNameDesc() {
-        return products.stream()
-                .sorted(Comparator.comparing(Product::getName).reversed())
-                .toList();
-    }
-
-    @Override
-    public double getTotalInventoryValue() {
-        return products.stream()
-                .mapToDouble(Product::getMaxRetailPrice)
-                .sum();
-    }
-
-    @Override
-    public double getTotalPriceAfterDiscount() {
-        return products.stream()
-                .mapToDouble(p -> p.getMaxRetailPrice() * (1 - p.getDiscountPercentage() / 100))
-                .sum();
-    }
-
-    @Override
-    public List<Product> getProductsAfterYear(int year) {
-        return products.stream()
-                .filter(p -> p.getManufacturedYear() > year)
-                .toList();
-    }
-
-    @Override
-    public List<Product> getAvailableAndPriceGreater(double price) {
-        return products.stream()
-                .filter(p -> p.isAvailable() && p.getMaxRetailPrice() > price)
-                .toList();
-    }
-
-    @Override
-    public Map<String, Long> countByCategory() {
-        return products.stream()
-                .collect(Collectors.groupingBy(Product::getCategory, Collectors.counting()));
-    }
-
-    @Override
-    public Map<String, List<Product>> groupByCategory() {
-        return products.stream()
-                .collect(Collectors.groupingBy(Product::getCategory));
-    }
-
-    @Override
-    public Map<String, List<Product>> groupByCompany() {
-        return products.stream()
-                .collect(Collectors.groupingBy(Product::getCompany));
-    }
-
-    @Override
-    public Map<Boolean, List<Product>> partitionByAvailability() {
-        return products.stream()
-                .collect(Collectors.partitioningBy(Product::isAvailable));
-    }
-
-    @Override
-    public Optional<Product> getMostExpensiveProduct() {
-        return products.stream()
-                .max(Comparator.comparing(Product::getMaxRetailPrice));
-    }
-
-    @Override
-    public Optional<Product> getCheapestProduct() {
-        return products.stream()
-                .min(Comparator.comparing(Product::getMaxRetailPrice));
-    }
-
-    @Override
-    public Map<Integer, Product> mapById() {
-        return products.stream()
-                .collect(Collectors.toMap(Product::getId, p -> p));
-    }
-
-    @Override
-    public Map<String, Double> avgPriceByCategory() {
-        return products.stream()
-                .collect(Collectors.groupingBy(
-                        Product::getCategory,
-                        Collectors.averagingDouble(Product::getMaxRetailPrice)
-                ));
-    }
-
-    @Override
-    public Map<String, List<Product>> top3ExpensiveByCategory() {
-        return products.stream()
-                .collect(Collectors.groupingBy(
-                        Product::getCategory,
-                        Collectors.collectingAndThen(
-                                Collectors.toList(),
-                                list -> list.stream()
-                                        .sorted(Comparator.comparing(Product::getMaxRetailPrice).reversed())
-                                        .limit(3)
-                                        .toList()
-                        )
-                ));
+    public List<Product> getProductByCategory() {
+        return List.of();
     }
 }
